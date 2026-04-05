aca5f9e83e1b153a5e6db69900dee25480c89dac
Sabrina Andrade <eusabrinaps@icloud.com>
2026-04-05 16:49:02 -0300

    Quebrando regra de negocio

--- src/main/java/PedidoService.java
+++ src/main/java/PedidoService.java
@@ -3,7 +3,7 @@ import java.util.UUID;
 public class PedidoService {
 
     public void calcularValorTotal(Pedido pedido){
-        pedido.setValorTotal(pedido.getProduto().getPreco() * pedido.getQuantidade());
+        pedido.setValorTotal(0);
     }
 
     public void gerarPedido(Produto produto, Cliente cliente, int quantidade){
